a9e1839b52166fc21bdf0ed93ef165bb35560b22
16413280 <16413280@qq.com>
2019-11-20 13:45:12 +0800

    v2

--- src/main/java/com/bdqn/dao/T1.java
+++ src/main/java/com/bdqn/dao/T1.java
@@ -1,4 +1,9 @@
 package com.bdqn.dao;
 
 public class T1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("ssss");
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Helssss是否第三方lo World!</h2>
+<h2>Helssss是否第三方lo Worldffff!</h2>
 </body>
 </html>
